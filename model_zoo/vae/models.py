c910bb402d6d3885ac3d9f90122c2f5bbf1f71c4
sammy-may <samuel.james.may1@gmail.com>
2025-04-14 13:54:01 -0700

    :sparkles: first working version of vae

--- model_zoo/vae/models.py
+++ model_zoo/vae/models.py
@@ -6,109 +6,79 @@ from model_zoo.utils import setup_logger
 logger = setup_logger()
 
 
-class LinearEncoder(nn.Module):
-    def __init__(self, input_dim, hidden_dim, latent_dim):
-        super(LinearEncoder, self).__init__()
-        self.input_dim = input_dim
-        self.hidden_dim = hidden_dim
-        self.latent_dim = latent_dim
-
-        self.fc_input = nn.Linear(input_dim, hidden_dim)
-        self.fc_hidden = nn.Linear(hidden_dim, hidden_dim)
-        self.latent_mean = nn.Linear(hidden_dim, latent_dim)
-        self.latent_logvar = nn.Linear(hidden_dim, latent_dim)
-        self.gelu = nn.GELU()
-
-    def forward(self, x):
-        x = self.gelu(self.fc_input(x))
-        x = self.gelu(self.fc_hidden(x))
-
-        mean = self.latent_mean(x)
-        logvar = self.latent_logvar(x)
-
-        return mean, logvar
-
-
-class LinearDecoder(nn.Module):
-    def __init__(self, output_dim, hidden_dim, latent_dim):
-        super(LinearDecoder, self).__init__()
-        self.output_dim = output_dim
-        self.hidden_dim = hidden_dim
-        self.latent_dim = latent_dim
-
-        self.fc_latent = nn.Linear(latent_dim, hidden_dim)
-        self.fc_hidden = nn.Linear(hidden_dim, hidden_dim)
-        self.fc_out = nn.Linear(hidden_dim, output_dim)
-        self.gelu = nn.GELU()
-
-    def forward(self, x):
-        x = self.gelu(self.fc_latent(x))
-        x = self.gelu(self.fc_hidden(x))
-        return torch.sigmoid(self.fc_out(x))
-
-
 class VAE(nn.Module):
-    def __init__(self, encoder, decoder):
+    def __init__(self, encoder: nn.Module, decoder: nn.Module):
         super(VAE, self).__init__()
         self.encoder = encoder
         self.decoder = decoder
 
-    def forward(self, x):
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
         mean, logvar = self.encoder(x)
-        noise = torch.randn_like(mean)
-        z = mean + (torch.exp(0.5 * logvar) * noise)
+        noise = torch.randn_like(logvar)
+        z = mean + noise * (torch.exp(0.5 * logvar))
         reconstruction = self.decoder(z)
 
         return reconstruction, mean, logvar
 
+    def generate(self, N: int = 1) -> torch.Tensor:
+        z = torch.randn(N, self.encoder.fc_mean.out_features)
+        return self.decoder(z)
+
 
 class Encoder(nn.Module):
-    def __init__(self, in_channels=1, latent_dim=20):
+    def __init__(self, in_channels: int = 1, latent_dim: int = 32):
         super(Encoder, self).__init__()
         self.conv1 = nn.Conv2d(
             in_channels, 16, kernel_size=4, stride=2, padding=1
         )  # 28x28 -> 14x14
+        self.batch_norm1 = nn.BatchNorm2d(16)
         self.conv2 = nn.Conv2d(
             16, 32, kernel_size=4, stride=2, padding=1
         )  # 14x14 -> 7x7
+        self.batch_norm2 = nn.BatchNorm2d(32)
         self.conv3 = nn.Conv2d(32, 32, kernel_size=3, stride=1, padding=1)  # 7x7 -> 7x7
+        self.batch_norm3 = nn.BatchNorm2d(32)
 
         self.flatten = nn.Flatten()
         self.fc_mean = nn.Linear(32 * 7 * 7, latent_dim)
         self.fc_logvar = nn.Linear(32 * 7 * 7, latent_dim)
-        self.gelu = nn.GELU()
+        self.activation = nn.GELU()
 
-    def forward(self, x):
-        x = self.gelu(self.conv1(x))
-        x = self.gelu(self.conv2(x))
-        x = self.gelu(self.conv3(x))
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.activation(self.batch_norm1(self.conv1(x)))
+        x = self.activation(self.batch_norm2(self.conv2(x)))
+        x = self.activation(self.batch_norm3(self.conv3(x)))
         x = self.flatten(x)
 
         mean = self.fc_mean(x)
         logvar = self.fc_logvar(x)
+
         return mean, logvar
 
 
 class Decoder(nn.Module):
-    def __init__(self, latent_dim=20, out_channels=1):
+    def __init__(self, latent_dim: int = 32, out_channels: int = 1):
         super(Decoder, self).__init__()
         self.fc = nn.Linear(latent_dim, 32 * 7 * 7)
-        self.gelu = nn.GELU()
+        self.activation = nn.GELU()
 
         self.deconv1 = nn.ConvTranspose2d(
             32, 32, kernel_size=3, stride=1, padding=1
         )  # 7x7
+        self.batch_norm1 = nn.BatchNorm2d(32)
         self.deconv2 = nn.ConvTranspose2d(
             32, 16, kernel_size=4, stride=2, padding=1
         )  # 7x7 -> 14x14
+        self.batch_norm2 = nn.BatchNorm2d(16)
         self.deconv3 = nn.ConvTranspose2d(
             16, out_channels, kernel_size=4, stride=2, padding=1
         )  # 14x14 -> 28x28
 
-    def forward(self, z):
-        x = self.gelu(self.fc(z))
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.activation(self.fc(x))
+
         x = x.view(-1, 32, 7, 7)
-        x = self.gelu(self.deconv1(x))
-        x = self.gelu(self.deconv2(x))
-        x = torch.sigmoid(self.deconv3(x))  # Output pixel values in [0,1]
-        return x
+
+        x = self.activation(self.batch_norm1(self.deconv1(x)))
+        x = self.activation(self.batch_norm2(self.deconv2(x)))
+        return torch.sigmoid(self.deconv3(x))  # bound pixel values in [0,1]
--- model_zoo/vae/vae.py
+++ model_zoo/vae/vae.py
@@ -2,6 +2,8 @@ import torch
 import torch.nn as nn
 import torchvision
 
+import matplotlib.pyplot as plt
+
 from model_zoo.utils import setup_logger
 from model_zoo.vae.models import VAE, Encoder, Decoder
 
@@ -43,15 +45,29 @@ class VAEHelper:
         )
 
     @staticmethod
-    def elbo_loss(x, x_reco, mean, logvar):
+    def elbo_loss(
+        x: torch.Tensor,
+        x_reco: torch.Tensor,
+        mean: torch.Tensor,
+        logvar: torch.Tensor,
+        beta: float,
+    ) -> torch.Tensor:
         reco_term = nn.functional.binary_cross_entropy(x_reco, x, reduction="mean")
         kl_term = -0.5 * torch.mean(1 + logvar - mean.pow(2) - logvar.exp())
-        return reco_term + kl_term
+        return reco_term + beta * kl_term
+
+    @staticmethod
+    def print_image(x: torch.Tensor, filepath: str):
+        """Save image to a pdf file specified by `filepath`."""
+        fig, ax = plt.subplots()
+        plt.imshow(x, cmap="gray")
+        plt.axis("off")
+        plt.savefig(filepath, format="pdf", bbox_inches="tight", pad_inches=0)
+        plt.close()
 
     def train(self):
         model = VAE(Encoder(), Decoder())
-
-        optimizer = torch.optim.AdamW(model.parameters(), lr=1e-3)
+        optimizer = torch.optim.AdamW(model.parameters(), lr=3e-4)
 
         for epoch in range(self.n_epochs):
             loss_epoch = 0
@@ -60,17 +76,35 @@ class VAEHelper:
                 optimizer.zero_grad()
                 X_reco, mean, logvar = model(X)
 
-                loss = self.elbo_loss(X, X_reco, mean, logvar)
+                loss = self.elbo_loss(
+                    X, X_reco, mean, logvar, 0.1 if epoch == 0 else 0.3
+                )
                 loss_epoch += loss.item()
 
                 loss.backward()
                 optimizer.step()
 
+            model.eval()
             loss_epoch *= 1.0 / (len(self.train_loader))
             print(epoch, loss_epoch)
 
+            with torch.no_grad():
+                self.print_image(
+                    X[-1, 0], self.data_path + "/vae_output/orig_{:d}.pdf".format(epoch)
+                )
+                self.print_image(
+                    X_reco[-1, 0],
+                    self.data_path + "/vae_output/reco_{:d}.pdf".format(epoch),
+                )
+                for i in range(3):
+                    self.print_image(
+                        model.generate()[0, 0],
+                        self.data_path
+                        + "/vae_output/gen_{:d}_{:d}.pdf".format(epoch, i),
+                    )
+
 
 if __name__ == "__main__":
-    vh = VAEHelper(32, 128)
-    vh.load_data()
-    vh.train()
+    vae = VAEHelper(latent_dim=32, batch_size=64)
+    vae.load_data()
+    vae.train()
